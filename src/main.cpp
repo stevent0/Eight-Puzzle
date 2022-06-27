4d02a2b15b4e55876fbc812460bc64b11282e805
unknown <truongsteven107@gmail.com>
2022-06-27 15:35:17 -0700

    Update

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,5 @@
 #include "../include/Solver.h"
 #include "../include/EightPuzzleNode.h"
-#include <iostream>
-#include <vector>
-#include <string>
-#include <queue>
-#include <unordered_set>
 
 using namespace std;
 
